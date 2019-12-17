8be38ba344de9268033915df062ec242865f2814
root <xiaycb@163.cn>
2019-12-17 11:50:18 +0800

    update

--- dubbodemo_consumer/src/main/java/com/itheima/controller/HelloController.java
+++ dubbodemo_consumer/src/main/java/com/itheima/controller/HelloController.java
@@ -5,7 +5,7 @@ import com.itheima.service.HelloService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+//hello
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
